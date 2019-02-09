455d3677726ee51983a85bda7b27f4808d0557e4
SprutSDM <zakoylov@gmail.com>
2019-02-09 19:22:52 +0300

    Documentation rest api and joinToQuest

--- cityorientation/rest_api.py
+++ cityorientation/rest_api.py
@@ -3,6 +3,8 @@ from flask import request
 from flask_restful import Resource,  Api
 
 api = Api(app)
+# Актуальная версия api
+version = 'v1.0'
 
 
 # Проверяет, что переданы корректные данные
@@ -52,6 +54,24 @@ class ListOfQuests(Resource):
         return {'answer': list_of_quests}
 
 
-api.add_resource(LoginTeam, '/api/loginTeam')
-api.add_resource(RenameTeam, '/api/renameTeam')
-api.add_resource(ListOfQuests, '/api/listOfQuests')
+# Принять участие в Квесте
+class JoinToQuest(Resource):
+    def post(self):
+        req = request.get_json()
+        ans = check_input_data(req, 'login', 'quest_id')
+        if ans != 'ok':
+            return {'answer': ans}
+        if db_quests.find_one({'quest_id': req['quest_id']}) is None:
+            return {'answer': 'quest does not exist'}
+        if db_teams.find_one({'login': req['login']}) is None:
+            return {'answer': 'team does not exist'}
+        if db_quests.find_one({'quest_id': req['quest_id'], 'teams': req['login']}) is not None:
+            return {'answer': 'team has already joined'}
+        db_quests.update({'quest_id': req['quest_id']}, {'$push': {'teams': req['login']}})
+        return {'answer': 'ok'}
+
+
+api.add_resource(LoginTeam, f'/api/{version}/loginTeam')
+api.add_resource(RenameTeam, f'/api/{version}/renameTeam')
+api.add_resource(ListOfQuests, f'/api/{version}/listOfQuests')
+api.add_resource(JoinToQuest, f'/api/{version}/joinToQuest')
